3a139aae31e145b98173bc47b90faa048b4ceb05
Peter Suggitt <me@suggs.org.uk>
2020-12-13 14:41:06 +0000

    completed day 2

---
+++ 2020/src/main/kotlin/org/suggs/adventofcode/Day2PasswordPhilosophy.kt
@@ -0,0 +1,42 @@
+package org.suggs.adventofcode
+
+/**
+ * @see https://adventofcode.com/2020/day/2
+ */
+class Day2PasswordPhilosophy(
+    private val range: Pair<Int, Int>,
+    private val testCharacter: Char,
+    private val password: String
+) {
+
+    companion object {
+        fun createPhilosophyFrom(passwordString: String): Day2PasswordPhilosophy {
+            val splits = passwordString.split(" ")
+            return Day2PasswordPhilosophy(pairOfIntsFrom(splits[0]), splits[1][0], splits[2])
+        }
+
+        private fun pairOfIntsFrom(range: String): Pair<Int, Int> {
+            val split = range.split("-")
+            return Pair(split[0].toInt(), split[1].toInt())
+        }
+    }
+
+    fun isValidByRange(): Boolean {
+        fun rangeOf(range: Pair<Int, Int>): IntRange {
+            return range.first.rangeTo(range.second)
+        }
+
+        return rangeOf(range).contains(password.filter { it == testCharacter }.count())
+    }
+
+    fun isValidByPosition(): Boolean {
+        // note the use of xor to check only one of 
+        return (password[range.first - 1] == testCharacter) xor (password[range.second - 1] == testCharacter)
+    }
+
+    override fun toString(): String {
+        return "Day2PasswordPhilosophy(range=$range, testCharacter=$testCharacter, password='$password')"
+    }
+
+
+}
---
+++ 2020/src/test/kotlin/org/suggs/adventofcode/Day2PasswordPhilosophyTest.kt
@@ -0,0 +1,63 @@
+package org.suggs.adventofcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import org.suggs.adventofcode.Day2PasswordPhilosophy.Companion.createPhilosophyFrom
+import java.io.File
+
+/**
+ * @see https://adventofcode.com/2020/day/2
+ */
+class Day2PasswordPhilosophyTest {
+
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    @Test
+    fun `count the number of valid passwords by range in a list`() {
+        val numberOfValidPasswords = countNumberOfValidPasswordsByRangeIn(createKnownSetOfNumbers())
+        log.info("Number of valid passwords by range in the known list is $numberOfValidPasswords")
+        assertThat(numberOfValidPasswords).isEqualTo(2)
+    }
+
+    @Test
+    fun `count the number of valid passwords by range in a file`(){
+        val numberOfValidPasswords = countNumberOfValidPasswordsByRangeIn(readPasswordsFromFile("day2-input.txt"))
+        log.info("Number of valid passwords by range in the file is $numberOfValidPasswords")
+        assertThat(numberOfValidPasswords).isEqualTo(643)
+    }
+
+    @Test
+    fun `count the number of valid passwords by placement in a list`(){
+        val numberOfValidPasswords = countNumberOfValidPasswordsByPositionIn(createKnownSetOfNumbers())
+        log.info("Number of valid passwords by position in the known list is $numberOfValidPasswords")
+        assertThat(numberOfValidPasswords).isEqualTo(1)
+    }
+
+    @Test
+    fun `count the number of valid passwords by placement in a file`(){
+        val numberOfValidPasswords = countNumberOfValidPasswordsByPositionIn(readPasswordsFromFile("day2-input.txt"))
+        log.info("Number of valid passwords by position in the file is $numberOfValidPasswords")
+        assertThat(numberOfValidPasswords).isEqualTo(388)
+    }
+
+    private fun countNumberOfValidPasswordsByRangeIn(listOfPasswords: List<Day2PasswordPhilosophy>): Int {
+        return listOfPasswords.filter { it.isValidByRange() }.size
+    }
+
+    private fun countNumberOfValidPasswordsByPositionIn(listOfPasswords: List<Day2PasswordPhilosophy>): Int {
+        return listOfPasswords.filter { it.isValidByPosition() }.size
+    }
+
+    private fun readPasswordsFromFile(nameOfFile: String): List<Day2PasswordPhilosophy> {
+        return File(ClassLoader.getSystemResource(nameOfFile).file).readLines().map { createPhilosophyFrom(it) }
+    }
+
+    private fun createKnownSetOfNumbers(): List<Day2PasswordPhilosophy> {
+        return listOf(
+            createPhilosophyFrom("1-3 a: abcde"),
+            createPhilosophyFrom("1-3 b: cdefg"),
+            createPhilosophyFrom("2-9 c: ccccccccc")
+        )
+    }
+}
