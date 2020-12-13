99deba5bda8b2292c82172b8de5f3c7eea9ac34c
Peter Suggitt <me@suggs.org.uk>
2020-12-13 12:36:00 +0000

    day 1 completed

---
+++ 2020/src/main/kotlin/org/suggs/adventofcode/day1/Day1ReportRepair.kt
@@ -0,0 +1,42 @@
+package org.suggs.adventofcode.day1
+
+class Day1ReportRepair() {
+
+    fun findTwoNumbersAndTimesThemFrom(listOfInts: List<Int>): Int {
+        return findAndMultiplyTwoFrom(listOfInts[0], listOfInts.drop(1))
+    }
+
+    private fun findAndMultiplyTwoFrom(head: Int, tail: List<Int>): Int {
+        if (tail.isEmpty()) return 0
+
+        val found = tail.find { it + head == 2020 }
+        return if (found != null) {
+            found * head
+        } else
+            findAndMultiplyTwoFrom(tail[0], tail.drop(1))
+    }
+
+    fun findThreeNumbersAndTimesThemFrom(listOfInts: List<Int>): Int {
+        return findThreeNumbersAndTimesThemFrom(listOfInts[0], listOfInts[1], listOfInts.drop(2))
+    }
+
+    private fun findThreeNumbersAndTimesThemFrom(head: Int, headToo: Int, tail: List<Int>): Int {
+        val combo = findAndMultiplyThreeFrom(head, headToo, tail)
+        return if (combo != 0)
+            combo
+        else
+            findThreeNumbersAndTimesThemFrom(headToo, tail[0], tail.drop(1))
+    }
+
+    private fun findAndMultiplyThreeFrom(head: Int, headToo: Int, tail: List<Int>): Int {
+        if (tail.isEmpty()) return 0
+
+        val found = tail.find { head + headToo + it == 2020 }
+        return if (found != null) {
+            head * headToo * found
+        } else
+            findAndMultiplyThreeFrom(head, tail[0], tail.drop(1))
+    }
+
+
+}
---
+++ 2020/src/test/kotlin/org/suggs/adventofcode/day1/Day1ReportRepairTest.kt
@@ -0,0 +1,49 @@
+package org.suggs.adventofcode.day1
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import java.io.File
+
+class Day1ReportRepairTest {
+
+    private val log = LoggerFactory.getLogger(this::class.java)
+    private val reportRepair = Day1ReportRepair()
+
+    @Test
+    fun `find two numbers in a list that add to 2020 from known list and multiply them`() {
+        val bigNumber = reportRepair.findTwoNumbersAndTimesThemFrom(createKnownSetOfNumbers())
+        log.info("Big number for two numbers from known list is $bigNumber")
+        assertThat(bigNumber).isEqualTo(514579)
+    }
+
+    @Test
+    fun `find two numbers in a list that add to 2020 from file and multiply them`() {
+        val bigNumber = reportRepair.findTwoNumbersAndTimesThemFrom(readNumbersFromFile("day1-input.txt"))
+        log.info("Big number for two numbers from file list is $bigNumber")
+        assertThat(bigNumber).isGreaterThan(1)
+    }
+
+    @Test
+    fun `find three numbers in a list that add to 2020 from known list and multiply them`() {
+        val bigNumber = reportRepair.findThreeNumbersAndTimesThemFrom(createKnownSetOfNumbers())
+        log.info("Big number for three numbers from known list is $bigNumber")
+        assertThat(bigNumber).isEqualTo(241861950)
+    }
+
+    @Test
+    fun `find three numbers in a list that add to 2020 from file and multiply them`() {
+        val bigNumber = reportRepair.findThreeNumbersAndTimesThemFrom(readNumbersFromFile("day1-input.txt"))
+        log.info("Big number for three numbers from file list is $bigNumber")
+        assertThat(bigNumber).isGreaterThan(1)
+    }
+
+
+    private fun readNumbersFromFile(nameOfFile: String): List<Int> {
+        return File(ClassLoader.getSystemResource(nameOfFile).file).readLines().map { it.toInt() }
+    }
+
+    private fun createKnownSetOfNumbers(): List<Int> {
+        return listOf(2004, 23, 123, 1721, 979, 366, 299, 675, 1456)
+    }
+}
