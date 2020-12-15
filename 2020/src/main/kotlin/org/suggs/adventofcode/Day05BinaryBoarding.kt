57e403b636feba829554fafd054135834ac80a62
Peter Suggitt <me@suggs.org.uk>
2020-12-15 10:04:52 +0000

    tidy up

--- 2020/src/main/kotlin/org/suggs/adventofcode/Day05BinaryBoarding.kt
+++ 2020/src/main/kotlin/org/suggs/adventofcode/Day05BinaryBoarding.kt
@@ -1,13 +1,8 @@
 package org.suggs.adventofcode
 
-import org.slf4j.LoggerFactory
-
 class Day05BinaryBoarding {
 
-    private val log = LoggerFactory.getLogger(this::class.java)
-
-    companion object{
-
+    companion object {
         private val charToBinaryLookup = mapOf('F' to '0', 'B' to '1', 'L' to 0, 'R' to '1')
 
         fun convertBoardingPassToDecimalSeat(boardingPass: String): Int {
