021a3de369083b08106a0dca10e5921f3ee03d9c
Peter Suggitt <me@suggs.org.uk>
2020-12-15 19:50:45 +0000

    wowzers ... only completed part 1 of day 7 .... jeez

--- 2020/src/main/kotlin/org/suggs/adventofcode/Day07HandyHaversacks.kt
+++ 2020/src/main/kotlin/org/suggs/adventofcode/Day07HandyHaversacks.kt
@@ -1,4 +1,48 @@
 package org.suggs.adventofcode
 
+import org.slf4j.LoggerFactory
+
 class Day07HandyHaversacks {
+
+    companion object {
+
+        private val log = LoggerFactory.getLogger(this::class.java)
+
+        private fun createRuleFrom(parent: String, number: Int, contains: String): BagRule {
+            return BagRule(parent, number, contains)
+        }
+
+        private fun cleanBagRules(rules: List<String>): List<String> {
+            return rules.filterNot { it.contains("no other bags") }.map { it.replace("""bags|bag|\.""".toRegex(), "") }
+        }
+
+        fun buildRulesFromRulesSet(rules: List<String>): List<BagRule> {
+            return cleanBagRules(rules).map {
+                val (left, right) = it.split(" contain ");
+                right.split(", ").map {
+                    val (num, bag) = it.split(" ", limit = 2)
+                    createRuleFrom(left.trim(), num.toInt(), bag.trim())
+                }
+            }.flatten()
+        }
+
+        fun buildMapOfRulesKeyedByChild(rules: List<String>): Map<String, List<BagRule>> {
+            return buildRulesFromRulesSet(rules).groupBy { it.child }
+        }
+
+        fun calculateDiscreteParentBagsFrom(rulesMap: Map<String, List<BagRule>>, bagName: String): Int {
+            return calculateDiscreteParentBagsFrom(rulesMap, bagName, listOf()).toSet().size
+        }
+
+        private fun calculateDiscreteParentBagsFrom(rulesMap: Map<String, List<BagRule>>, bagName: String, acc: List<String>): List<String> {
+            return if(!rulesMap.containsKey(bagName))
+                acc
+            else
+                rulesMap.getValue(bagName).map {
+                    calculateDiscreteParentBagsFrom(rulesMap, it.parent, acc + it.parent)
+                }.flatten()
+        }
+    }
+
+    data class BagRule(val parent: String, val number: Int, val child: String)
 }
--- 2020/src/test/kotlin/org/suggs/adventofcode/Day07HandyHaversacksTest.kt
+++ 2020/src/test/kotlin/org/suggs/adventofcode/Day07HandyHaversacksTest.kt
@@ -1,8 +1,54 @@
 package org.suggs.adventofcode
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import org.suggs.adventofcode.Day07HandyHaversacks.Companion.buildMapOfRulesKeyedByChild
+import org.suggs.adventofcode.Day07HandyHaversacks.Companion.buildRulesFromRulesSet
+import org.suggs.adventofcode.Day07HandyHaversacks.Companion.calculateDiscreteParentBagsFrom
 import java.io.File
 
 class Day07HandyHaversacksTest {
+
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    @Test
+    fun `can parse a set list of bag descriptions into a collection of rules`() {
+        val rulesList = buildRulesFromRulesSet(testBags)
+        assertThat(rulesList.size).isEqualTo(13)
+    }
+
+    @Test
+    fun `can create a map of rules keyed from the child object`(){
+        val rulesMap = buildMapOfRulesKeyedByChild(testBags)
+        assertThat(rulesMap.values.size).isEqualTo(7)
+        assertThat(rulesMap.values.flatten().size).isEqualTo(13)
+    }
+
+    @Test
+    fun `can parse a large number of bag descriptions into a large collection of rules`(){
+        val rulesList = buildRulesFromRulesSet(readBags)
+        assertThat(rulesList.size).isEqualTo(1457)
+    }
+
+    @Test
+    fun `can create a map of a large number of bag rules keyed from the child object`(){
+        val rulesMap = buildMapOfRulesKeyedByChild(readBags)
+        assertThat(rulesMap.values.size).isEqualTo(413)
+        assertThat(rulesMap.values.flatten().size).isEqualTo(1457)
+    }
+
+    @Test
+    fun `can calculate the number of possible outer bags that shiny gold could reside in from graph`(){
+        val rulesMap = buildMapOfRulesKeyedByChild(testBags)
+        assertThat(calculateDiscreteParentBagsFrom(rulesMap, "shiny gold")).isEqualTo(4)
+    }
+
+    @Test
+    fun `can calculate the number of possible outer bags that shiny gold could reside in from huge graph`(){
+        val rulesMap = buildMapOfRulesKeyedByChild(readBags)
+        assertThat(calculateDiscreteParentBagsFrom(rulesMap, "shiny gold")).isEqualTo(332)
+    }
 
     private val readBags: List<String> = File(ClassLoader.getSystemResource("day07-input.txt").file).readLines()
 
