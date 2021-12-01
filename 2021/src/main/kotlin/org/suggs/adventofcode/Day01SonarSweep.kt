7a7756c4ecaa07f8de75c4b08d5124f8ce033685
Peter Suggitt <me@suggs.org.uk>
2021-12-01 17:02:45 +0000

    completed second part of day 2

--- 2021/src/main/kotlin/org/suggs/adventofcode/Day01SonarSweep.kt
+++ 2021/src/main/kotlin/org/suggs/adventofcode/Day01SonarSweep.kt
@@ -8,11 +8,25 @@ object Day01SonarSweep {
 
     private fun countNumberOfIncrementsFrom(listOfNumbers: List<Int>, counter: Int): Int {
         return if (listOfNumbers.size < 2) counter
-        else countNumberOfIncrementsFrom(listOfNumbers.drop(1), counter + checkHeadIsIncrement(listOfNumbers.take(2)))
+        else countNumberOfIncrementsFrom(listOfNumbers.drop(1), counter + compareHeadForIncrement(listOfNumbers.take(2)))
     }
 
-    private fun checkHeadIsIncrement(head: List<Int>): Int {
+    private fun compareHeadForIncrement(head: List<Int>): Int {
         return if (head[0] < head[1]) return 1
+        else 0
+    }
+
+    fun countNumberOfThreeNumberIncrementsFrom(listOfNumbers: List<Int>): Int {
+        return countNumberOfThreeNumberIncrementsFrom(listOfNumbers, 0)
+    }
+
+    fun countNumberOfThreeNumberIncrementsFrom(listOfNumbers: List<Int>, counter: Int): Int {
+        return if (listOfNumbers.size < 4) counter
+        else countNumberOfThreeNumberIncrementsFrom(listOfNumbers.drop(1), counter + compareHeadThreeForIncrement(listOfNumbers.take(4)))
+    }
+
+    private fun compareHeadThreeForIncrement(head: List<Int>): Int {
+        return if (head.take(3).sumOf { it } < head.drop(1).sumOf { it }) 1
         else 0
     }
 
--- 2021/src/test/kotlin/org/suggs/adventofcode/Day01SonarSweepTest.kt
+++ 2021/src/test/kotlin/org/suggs/adventofcode/Day01SonarSweepTest.kt
@@ -4,6 +4,7 @@ import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
 import org.suggs.adventofcode.Day01SonarSweep.countNumberOfIncrementsFrom
+import org.suggs.adventofcode.Day01SonarSweep.countNumberOfThreeNumberIncrementsFrom
 import org.suggs.adventofcode.Util.createIntListFrom
 
 internal class Day01SonarSweepTest {
@@ -20,6 +21,16 @@ internal class Day01SonarSweepTest {
     @Test
     fun `counts the number of increasing numbers in a very bog list`() {
         countNumberOfIncrementsFrom(readArray) shouldBe 1791
+    }
+
+    @Test
+    fun `counts the number of increasing numbers from a three count increments in a small list`() {
+        countNumberOfThreeNumberIncrementsFrom(verySmallArray) shouldBe 5
+    }
+
+    @Test
+    fun `counts the number of increasing numbers from a three count increments in a very big list`() {
+        countNumberOfThreeNumberIncrementsFrom(readArray) shouldBe 1822
     }
 
     private val readArray = createIntListFrom("day01-input.txt")
