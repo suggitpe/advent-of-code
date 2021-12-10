ff513d936ebb8b6310cea191e2fa9b1bee839c46
Peter Suggitt <me@suggs.org.uk>
2021-12-10 07:07:00 +0000

    small refactor

--- 2021/src/main/kotlin/org/suggs/adventofcode/Day09SmokeBasin.kt
+++ 2021/src/main/kotlin/org/suggs/adventofcode/Day09SmokeBasin.kt
@@ -7,18 +7,23 @@ object Day09SmokeBasin {
     private val log = LoggerFactory.getLogger(this::class.java)
 
     fun countRiskLevelsInMatrixFrom(dataSet: Array<Array<Int>>): Int {
+
+        fun at(x: Int, y: Int) = dataSet[y][x]
+
         fun isValidReference(x: Int, y: Int) = x >= 0 && x < dataSet.first().size && y >= 0 && y < dataSet.size
 
         fun checkLowest(x: Int, y: Int, digit: Int) = when {
             !isValidReference(x, y) -> true
-            dataSet[y][x] > digit -> true
+            at(x, y) > digit -> true
             else -> false
         }
 
         fun isDigitLowest(digit: Int, x: Int, y: Int): Boolean =
-            checkLowest(x - 1, y, digit) && checkLowest(x, y - 1, digit) && checkLowest(x + 1, y, digit) && checkLowest(x, y + 1, digit)
+            checkLowest(x - 1, y, digit)
+                    && checkLowest(x, y - 1, digit)
+                    && checkLowest(x + 1, y, digit)
+                    && checkLowest(x, y + 1, digit)
 
-//        print(dataSet)
         val totalCountOfRiskLevels = mutableListOf<Int>()
         dataSet.forEachIndexed { y, ints ->
             ints.forEachIndexed { x, digit ->
@@ -30,7 +35,4 @@ object Day09SmokeBasin {
         return totalCountOfRiskLevels.sum()
     }
 
-    private fun print(dataSet: Array<Array<Int>>) {
-        dataSet.map { it -> log.debug("${it.map { it }}") }
-    }
 }
