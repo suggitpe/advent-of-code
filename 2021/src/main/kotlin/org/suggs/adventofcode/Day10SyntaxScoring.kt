dbfb7851775e12f7a5758659020b8b4aa31c6c06
Peter Suggitt <me@suggs.org.uk>
2021-12-10 16:18:17 +0000

    completed day 2 ... dont like the solution

--- 2021/src/main/kotlin/org/suggs/adventofcode/Day10SyntaxScoring.kt
+++ 2021/src/main/kotlin/org/suggs/adventofcode/Day10SyntaxScoring.kt
@@ -7,6 +7,28 @@ object Day10SyntaxScoring {
     private val log = LoggerFactory.getLogger(this::class.java)
     private val charMap: Map<Char, Char> = mapOf('{' to '}', '<' to '>', '[' to ']', '(' to ')')
     private val valueMap: Map<Char, Int> = mapOf(')' to 3, ']' to 57, '}' to 1197, '>' to 25137)
+    private val closeValueMap: Map<Char, Int> = mapOf(')' to 1, ']' to 2, '}' to 3, '>' to 4)
+
+    fun calculateScoreOfRemainder(dataSet: List<String>): Long {
+        val foo = dataSet.asSequence().map { removeNestedPairs(it) }.filter { isValid(it) }.map { createClosingSyntaxFrom(it) }.map { calculateValueOf(it) }.map { it }.sorted().toList()
+        return foo[foo.size / 2]
+    }
+
+    private fun calculateValueOf(switchedSyntax: String): Long {
+        fun calculateValueOf(switchedSyntax: CharArray, aggregate: Long): Long {
+            return when {
+                switchedSyntax.isEmpty() -> aggregate
+                else -> calculateValueOf(switchedSyntax.drop(1).toCharArray(), (aggregate * 5) + closeValueMap[switchedSyntax.first()]!!)
+            }
+        }
+        return calculateValueOf(switchedSyntax.toCharArray(), 0L)
+    }
+
+    private fun createClosingSyntaxFrom(remainder: String): String =
+        remainder.reversed().map { charMap[it]!! }.joinToString("")
+
+    private fun isValid(syntax: String) =
+        syntax.filterNot { charMap.contains(it) }.isEmpty()
 
     fun calculateSyntaxScoreFrom(dataSet: List<String>) =
         dataSet.sumOf { calculateSyntaxScoreFrom(removeNestedPairs(it)) }
@@ -20,15 +42,15 @@ object Day10SyntaxScoring {
     }
 
     private fun removeNestedPairs(original: String): String {
-
         fun removePairsFrom(syntax: String) =
             syntax.replace("<>", "").replace("()", "").replace("[]", "").replace("{}", "")
 
         val cleaned = removePairsFrom(original)
-        return if (cleaned.length == original.length)
-            return cleaned
-        else
-            removeNestedPairs(cleaned)
+        return when (cleaned.length) {
+            original.length -> cleaned
+            else -> removeNestedPairs(cleaned)
+        }
+
     }
 
 
--- 2021/src/test/kotlin/org/suggs/adventofcode/Day10SyntaxScoringTest.kt
+++ 2021/src/test/kotlin/org/suggs/adventofcode/Day10SyntaxScoringTest.kt
@@ -1,9 +1,10 @@
 package org.suggs.adventofcode
 
 import io.kotest.matchers.ints.shouldBeInRange
+import io.kotest.matchers.longs.shouldBeInRange
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import org.suggs.adventofcode.Day10SyntaxScoring.calculateScoreOfRemainder
 import org.suggs.adventofcode.Day10SyntaxScoring.calculateSyntaxScoreFrom
 import org.suggs.adventofcode.Util.getFileLinesFrom
 
@@ -17,6 +18,19 @@ class Day10SyntaxScoringTest {
 
     @Test
     fun `calculates syntax score from large data set`() = calculateSyntaxScoreFrom(readDataSet) shouldBeInRange 343000..344000
+
+    @Test
+    fun `calculates the core of the remainder of unfinished sequence for a single line`() =
+        calculateScoreOfRemainder(listOf("(((({<>}<{<{<>}{[]{[]{}")) shouldBe 1480781
+
+    @Test
+    fun `calculates the core of the remainder of unfinished sequence for a sample data set`() =
+        calculateScoreOfRemainder(sampleDataSet) shouldBe 288957
+
+    @Test
+    fun `calculates the core of the remainder of unfinished sequence for a large data set`() =
+        calculateScoreOfRemainder(readDataSet) shouldBeInRange 2924730000..2924740000
+
 
     private val readDataSet: List<String> = getFileLinesFrom("day10-input.txt")
     private val sampleDataSet: List<String> = """[({(<(())[]>[[{[]{<()<>>
