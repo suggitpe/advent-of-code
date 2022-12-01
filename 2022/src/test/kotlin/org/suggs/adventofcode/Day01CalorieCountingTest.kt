4b6fed440f72e3fd6baf970a97ef47bd6ca39470
Peter Suggitt <me@suggs.org.uk>
2022-12-01 17:10:14 +0000

    adopted kotest

--- 2022/src/test/kotlin/org/suggs/adventofcode/Day01CalorieCountingTest.kt
+++ 2022/src/test/kotlin/org/suggs/adventofcode/Day01CalorieCountingTest.kt
@@ -1,31 +1,31 @@
 package org.suggs.adventofcode
 
-import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.core.spec.style.FeatureSpec
 import io.kotest.matchers.shouldBe
 import org.suggs.adventofcode.Day01CalorieCounting.findMaxCaloriesFrom
 import org.suggs.adventofcode.Day01CalorieCounting.findMaxCaloriesOfTopThreeFrom
 import org.suggs.adventofcode.Util.getTextBlocksFrom
 
-class Day01CalorieCountingTest : ShouldSpec({
+class Day01CalorieCountingTest : FeatureSpec({
 
     val smallData: List<String> = getTextBlocksFrom("day01-unit.txt")
     val largeData: List<String> = getTextBlocksFrom("day01-input.txt")
 
-    context("Calculate calorie counts for a set of imaginary elves") {
+    feature("Calculate calorie counts for a set of imaginary elves") {
 
-        should("find the maximum calories for a small set of data") {
+        scenario("find the maximum calories for a small set of data") {
             findMaxCaloriesFrom(smallData) shouldBe 24000
         }
 
-        should("find max calories from larger list") {
+        scenario("find max calories from larger list") {
             findMaxCaloriesFrom(largeData) shouldBe 70720
         }
 
-        should("find calorie count of top three from small list") {
+        scenario("find calorie count of top three from small list") {
             findMaxCaloriesOfTopThreeFrom(smallData) shouldBe 45000
         }
 
-        should("find calorie count of top three from larger list") {
+        scenario("find calorie count of top three from larger list") {
             findMaxCaloriesOfTopThreeFrom(largeData) shouldBe 207148
         }
     }
