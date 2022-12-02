7beb5b7376686fd481e00ae8811d4667993ee7fe
Peter Suggitt <me@suggs.org.uk>
2022-12-02 07:48:30 +0000

    completed task like a brute

--- 2022/src/main/kotlin/org/suggs/adventofcode/Day02RockPaperScissors.kt
+++ 2022/src/main/kotlin/org/suggs/adventofcode/Day02RockPaperScissors.kt
@@ -17,6 +17,22 @@ object Day02RockPaperScissors {
         "C Z" to 6  // 3 + 3
     )
 
+    // x == lose, y == draw, z == win
+    private val other = mapOf<String, Int>(
+        "A X" to 3, // 3 + 0
+        "A Y" to 4, // 1 + 3 *
+        "A Z" to 8, // 2 + 6
+        "B X" to 1, // 1 + 0 *
+        "B Y" to 5, // 2 + 3 *
+        "B Z" to 9, // 3 + 6 *
+        "C X" to 2, // 2 + 0 *
+        "C Y" to 6, // 3 + 3 *
+        "C Z" to 7  // 1 + 6 *
+    )
+
     fun playRockPaperScissorsWith(data: List<String>) =
         data.sumOf { scores[it]!! }
+
+    fun playRockPaperToWin(data: List<String>) =
+        data.sumOf { other[it]!! }
 }
--- 2022/src/test/kotlin/org/suggs/adventofcode/Day02RockPaperScissorsTest.kt
+++ 2022/src/test/kotlin/org/suggs/adventofcode/Day02RockPaperScissorsTest.kt
@@ -3,6 +3,7 @@ package org.suggs.adventofcode
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import org.suggs.adventofcode.Day02RockPaperScissors.playRockPaperScissorsWith
+import org.suggs.adventofcode.Day02RockPaperScissors.playRockPaperToWin
 import org.suggs.adventofcode.Util.getFileLinesFrom
 
 class Day02RockPaperScissorsTest {
@@ -18,5 +19,14 @@ class Day02RockPaperScissorsTest {
     @Test
     fun `play rock paper scissors with larger data`() =
         playRockPaperScissorsWith(largeData) shouldBe 12458
+
+    @Test
+    fun `play rock paper scissors to win with small data`() =
+        playRockPaperToWin(smallData) shouldBe 12
+
+
+    @Test
+    fun `play rock paper scissors to win with larger data`() =
+        playRockPaperToWin(largeData) shouldBe 12683
 
 }
