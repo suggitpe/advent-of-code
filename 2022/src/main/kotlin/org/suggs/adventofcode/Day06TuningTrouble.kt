2985ca48def62225f7501dc741e351192d74dc15
Peter Suggitt <me@suggs.org.uk>
2022-12-06 08:11:06 +0000

    slightly optimised version

--- 2022/src/main/kotlin/org/suggs/adventofcode/Day06TuningTrouble.kt
+++ 2022/src/main/kotlin/org/suggs/adventofcode/Day06TuningTrouble.kt
@@ -2,18 +2,18 @@ package org.suggs.adventofcode
 
 object Day06TuningTrouble {
 
-    fun identifyMarkerLocationFrom(data: String, sizeOfMarker: Int): Int {
-        return data.indexOf(findUniqueLetters(data, sizeOfMarker)) + sizeOfMarker
-    }
+    fun identifyMarkerLocationFrom(data: String, sizeOfMarker: Int) =
+        data.length - countOfLettersAfterMarkerFrom(data, sizeOfMarker)
 
-    private fun findUniqueLetters(data: String, sizeOfMarker: Int): String {
+    // love a bit of recursion
+    private fun countOfLettersAfterMarkerFrom(data: String, sizeOfMarker: Int): Int {
         return if (areCharactersUniqueFrom(data.take(sizeOfMarker)))
-            data.take(sizeOfMarker)
+            return data.length - sizeOfMarker
         else
-            findUniqueLetters(data.drop(1), sizeOfMarker)
+            countOfLettersAfterMarkerFrom(data.drop(1), sizeOfMarker)
     }
 
-    private fun areCharactersUniqueFrom(data: String): Boolean {
-        return data.toSet().size == data.length
-    }
+    private fun areCharactersUniqueFrom(data: String) =
+        data.toSet().size == data.length
+
 }
