9b050bba2dae4165f8c13c8ee9bd477364e91d0e
Peter Suggitt <me@suggs.org.uk>
2023-12-01 09:41:30 +0000

    day 1 solved

--- 2023/src/main/kotlin/org/suggs/adventofcode/Day01Trebuchet.kt
+++ 2023/src/main/kotlin/org/suggs/adventofcode/Day01Trebuchet.kt
@@ -1,8 +1,13 @@
 package org.suggs.adventofcode
 
+import org.slf4j.LoggerFactory
+
 object Day01Trebuchet {
 
-    fun calculateTotalCalibrationValuesFrom(dirtyCalibrationData: String): Int {
-        TODO("Not yet implemented")
-    }
+    private val TRIM_DIGIT_REGEX = "[^\\d.]".toRegex()
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    fun calculateTotalCalibrationValuesFrom(dirtyCalibrationData: List<String>) =
+        dirtyCalibrationData.map { it.replace(TRIM_DIGIT_REGEX, "") }.map { "${it.first()}${it.last()}" }.sumOf { it.toInt() }
+
 }
--- 2023/src/test/kotlin/org/suggs/adventofcode/Day01TrebuchetTest.kt
+++ 2023/src/test/kotlin/org/suggs/adventofcode/Day01TrebuchetTest.kt
@@ -3,17 +3,25 @@ package org.suggs.adventofcode
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.suggs.adventofcode.Day01Trebuchet.calculateTotalCalibrationValuesFrom
 
 @DisplayName("Trebuchet?!")
 class Day01TrebuchetTest {
 
     @Test
-    fun `calculates calibration values from strings (small data)`() =
-        Day01Trebuchet.calculateTotalCalibrationValuesFrom(smallData) shouldBe 142
+    fun `calculates calibration values from strings (small data)`() {
+        calculateTotalCalibrationValuesFrom(smallData) shouldBe 142
+    }
+
+    @Test
+    fun `calculates calibration values from external data`() {
+        calculateTotalCalibrationValuesFrom(largeData) shouldBe 123
+    }
 
 
+    private val largeData = Util.getFileLinesFrom("day01-input.txt")
     private val smallData = """1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
-treb7uchet"""
+treb7uchet""".split("\n")
 }
