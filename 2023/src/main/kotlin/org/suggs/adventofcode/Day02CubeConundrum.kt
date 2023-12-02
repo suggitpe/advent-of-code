7ef75ca3aeed4775ed4b4e4d2bbfb4035f44f00f
Peter Suggitt <me@suggs.org.uk>
2023-12-02 11:15:23 +0000

    day 2 part 1 done

--- 2023/src/main/kotlin/org/suggs/adventofcode/Day02CubeConundrum.kt
+++ 2023/src/main/kotlin/org/suggs/adventofcode/Day02CubeConundrum.kt
@@ -5,8 +5,45 @@ import org.slf4j.LoggerFactory
 object Day02CubeConundrum {
     private val log = LoggerFactory.getLogger(this::class.java)
 
-    fun sumIdsOfValidGamesFrom(smallData: List<String>): Int {
-        return 8
+    fun sumIdsOfValidGamesFrom(gamesData: List<String>) =
+        buildGamesFrom(gamesData, listOf())
+            .filter { it.sets.none { set -> set.blue > 14 || set.red > 12 || set.green > 13 } }
+            .sumOf { it.id }
+
+    private fun buildGamesFrom(gamesData: List<String>, games: List<Game>): List<Game> {
+        return if (gamesData.isEmpty()) games
+        else buildGamesFrom(gamesData.drop(1), games + Game(gamesData.first()))
     }
 
+}
+
+data class Game(val id: Int, val sets: List<GameSet>) {
+    companion object {
+        operator fun invoke(gameData: String): Game {
+            return Game(
+                gameData.split(":").first().split(" ").last().toInt(),
+                createGameSetsFrom(gameData.split(":").last().split(";"), listOf())
+            )
+        }
+
+        private fun createGameSetsFrom(setData: List<String>, gameSets: List<GameSet>): List<GameSet> {
+            return if (setData.isEmpty()) gameSets
+            else createGameSetsFrom(setData.drop(1), gameSets + GameSet(setData.first()))
+        }
+    }
+}
+
+data class GameSet(val red: Int, val blue: Int, val green: Int) {
+    companion object {
+        operator fun invoke(setData: String) =
+            GameSet(getColourCountFrom(setData, "red"), getColourCountFrom(setData, "blue"), getColourCountFrom(setData, "green"))
+
+        private fun getColourCountFrom(setData: String, colour: String) =
+            extractCountFrom(setData.split(", ").map { it.trim() }.filter { it.contains(colour) })
+
+        private fun extractCountFrom(setColour: List<String>): Int {
+            return if (setColour.isEmpty()) 0
+            else setColour.first().split(" ").first().toInt()
+        }
+    }
 }
