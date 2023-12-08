643b7d657e98c89a8bf69e97deca5378799047ae
Peter Suggitt <me@suggs.org.uk>
2023-12-08 19:12:57 +0000

    day 2 done ,,, brute force did not work

--- 2023/src/main/kotlin/org/suggs/adventofcode/Day08HauntedWasteland.kt
+++ 2023/src/main/kotlin/org/suggs/adventofcode/Day08HauntedWasteland.kt
@@ -6,27 +6,50 @@ object Day08HauntedWasteland {
     private val log = LoggerFactory.getLogger(this::class.java)
 
     fun countHopsInDataMap(data: List<String>) =
-        countHopsToFinsh("AAA", data.first(), data.last().split("\n").associate { it.split(" ").first() to Hop(it) }, 0)
+        countHopsToFinsh("AAA", data.first(), buildHopsDataMapFrom(data), 0)
             .also { log.debug("Part 1: $it") }
 
-    private tailrec fun countHopsToFinsh(key: String, indicator: String, hops: Map<String, Hop>, acum: Int): Int {
-        return if (key == "ZZZ") acum
-        else countHopsToFinsh(hops[key]!!.get(indicator.first()), shuffleIndicators(indicator), hops, acum + 1)
+    private tailrec fun countHopsToFinsh(key: String, indicator: String, hopMap: Map<String, Hop>, accumulator: Long): Long {
+        return if (key == "ZZZ") accumulator
+        else countHopsToFinsh(hopMap[key]!!.get(indicator.first()), shuffleIndicators(indicator), hopMap, accumulator + 1L)
+    }
+
+    fun countHopsInDataMapForMultiHop(data: List<String>): Long {
+        val hopMap = buildHopsDataMapFrom(data)
+        return hopMap.keys.filter { it.endsWith("A") }
+            .map { countHopsToFinshEndingZ(it, data.first(), buildHopsDataMapFrom(data), 0) }
+            .reduce { acc, next -> lcm(acc, next) }
+            .also { log.debug("Part 2: $it") }
+    }
+
+    private fun lcm(a: Long, b: Long) = a / gcd(a, b) * b
+
+    private fun gcd(a: Long, b: Long): Long {
+        return if (b == 0L) a
+        else gcd(b, a % b)
+    }
+
+    private tailrec fun countHopsToFinshEndingZ(key: String, indicator: String, hopMap: Map<String, Hop>, accumulator: Long): Long {
+        return if (key.endsWith("Z")) accumulator
+        else countHopsToFinshEndingZ(hopMap[key]!!.get(indicator.first()), shuffleIndicators(indicator), hopMap, accumulator + 1L)
     }
 
     private fun shuffleIndicators(indicator: String) = indicator.drop(1) + indicator.first()
 
-    data class Hop(val left: String, val right: String) {
+    private fun buildHopsDataMapFrom(data: List<String>) =
+        data.last().split("\n").associate { it.split(" ").first() to Hop(it) }
+}
 
-        companion object {
-            operator fun invoke(hopData: String): Hop {
-                return Hop(
-                    hopData.split("(")[1].split(",").first().trim(),
-                    hopData.split("(")[1].split(",")[1].split(")").first().trim()
-                )
-            }
+data class Hop(val left: String, val right: String) {
+
+    companion object {
+        operator fun invoke(hopData: String): Hop {
+            return Hop(
+                hopData.split("(")[1].split(",").first().trim(),
+                hopData.split("(")[1].split(",")[1].split(")").first().trim()
+            )
         }
-
-        fun get(indicator: Char) = if (indicator == 'L') left else right
     }
+
+    fun get(indicator: Char) = if (indicator == 'L') left else right
 }
--- 2023/src/test/kotlin/org/suggs/adventofcode/Day08HauntedWastelandTest.kt
+++ 2023/src/test/kotlin/org/suggs/adventofcode/Day08HauntedWastelandTest.kt
@@ -1,9 +1,11 @@
 package org.suggs.adventofcode
 
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.suggs.adventofcode.Day08HauntedWasteland.countHopsInDataMap
+import org.suggs.adventofcode.Day08HauntedWasteland.countHopsInDataMapForMultiHop
 import org.suggs.adventofcode.Util.getTextBlocksFrom
 
 @DisplayName("Haunted Wasteland")
@@ -11,19 +13,42 @@ class Day08HauntedWastelandTest {
 
     @Test
     fun `part 1 - count hops to finsh using small data`() {
-        countHopsInDataMap(smallData) shouldBe 6
+        countHopsInDataMap(smallData1) shouldBe 6
     }
 
     @Test
+    @Disabled
     fun `part 1 - count hops to finsh using large data`() {
         countHopsInDataMap(largeData) shouldBe 123
     }
 
+    @Test
+    fun `part 2 - count hops to finish for multi hop with small data`() {
+        countHopsInDataMapForMultiHop(smallData2) shouldBe 6
+    }
+
+    @Test
+    @Disabled
+    fun `part 2 - count hops to finish for multi hop with large data`() {
+        countHopsInDataMapForMultiHop(largeData) shouldBe 123
+    }
+
     private val largeData = getTextBlocksFrom("day08-input.txt")
 
-    private val smallData = """LLR
+    private val smallData1 = """LLR
 
 AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)""".split("\n\n")
+
+    private val smallData2 = """LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)""".split("\n\n")
 }
