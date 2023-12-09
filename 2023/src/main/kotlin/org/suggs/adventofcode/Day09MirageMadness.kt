22e6c42495e0669fc848441fd773ba5b29a290a3
Peter Suggitt <me@suggs.org.uk>
2023-12-09 12:54:21 +0000

    cant help but tidy it up a bit

--- 2023/src/main/kotlin/org/suggs/adventofcode/Day09MirageMadness.kt
+++ 2023/src/main/kotlin/org/suggs/adventofcode/Day09MirageMadness.kt
@@ -7,23 +7,19 @@ object Day09MirageMadness {
     private val log = LoggerFactory.getLogger(this::class.java)
 
     fun sumIncrementsFrom(data: List<String>) =
-        data.map { it.split("\\s+".toRegex()).map { foo -> foo.toInt() } }
-            .sumOf { calculateNextInSequenceFor(it) }
-            .also { log.debug("Part 1: $it") }
+        buildSequencesFrom(data).sumOf { calculateNextInSequenceFor(it) }.also { log.debug("Part 1: $it") }
 
-    private fun calculateNextInSequenceFor(sequence: List<Int>): Int {
-        return if (sequence.all { it == 0 }) 0
-        else calculateNextInSequenceFor(sequence.zipWithNext { a, b -> b - a }) + sequence.last()
-    }
+    private fun calculateNextInSequenceFor(sequence: List<Int>): Int =
+        if (sequence.all { it == 0 }) 0
+        else (calculateNextInSequenceFor(sequence.zipWithNext { a, b -> b - a }) + sequence.last())
 
-    fun sumPriorSequenceFrom(data: List<String>): Int {
-        return data.map { it.split("\\s+".toRegex()).map { foo -> foo.toInt() } }
-            .sumOf { calculateFirstInSequenceFor(it) }
-            .also { log.debug("Part 2: $it") }
-    }
+    fun sumPriorSequenceFrom(data: List<String>): Int =
+        buildSequencesFrom(data).sumOf { calculateFirstInSequenceFor(it) }.also { log.debug("Part 2: $it") }
 
-    private fun calculateFirstInSequenceFor(sequence: List<Int>): Int {
-        return if (sequence.all { it == 0 }) 0
+    private fun calculateFirstInSequenceFor(sequence: List<Int>): Int =
+        if (sequence.all { it == 0 }) 0
         else sequence.first() - calculateFirstInSequenceFor(sequence.zipWithNext { a, b -> b - a })
-    }
+
+    private fun buildSequencesFrom(data: List<String>): List<List<Int>> =
+        data.map { it.split("\\s+".toRegex()).map { foo -> foo.toInt() } }
 }
