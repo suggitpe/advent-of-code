89a98a448a24851ccc114330672eff3636f4f239
Peter Suggitt <me@suggs.org.uk>
2023-12-09 13:01:12 +0000

    tidy

--- 2023/src/main/kotlin/org/suggs/adventofcode/Day09MirageMadness.kt
+++ 2023/src/main/kotlin/org/suggs/adventofcode/Day09MirageMadness.kt
@@ -7,19 +7,26 @@ object Day09MirageMadness {
     private val log = LoggerFactory.getLogger(this::class.java)
 
     fun sumIncrementsFrom(data: List<String>) =
-        buildSequencesFrom(data).sumOf { calculateNextInSequenceFor(it) }.also { log.debug("Part 1: $it") }
+        buildSequencesFrom(data)
+            .sumOf { calculateNextInSequenceFor(it) }
+            .also { log.debug("Part 1: $it") }
 
     private fun calculateNextInSequenceFor(sequence: List<Int>): Int =
         if (sequence.all { it == 0 }) 0
         else (calculateNextInSequenceFor(sequence.zipWithNext { a, b -> b - a }) + sequence.last())
 
     fun sumPriorSequenceFrom(data: List<String>): Int =
-        buildSequencesFrom(data).sumOf { calculateFirstInSequenceFor(it) }.also { log.debug("Part 2: $it") }
+        buildSequencesFrom(data)
+            .sumOf { calculateFirstInSequenceFor(it) }
+            .also { log.debug("Part 2: $it") }
 
     private fun calculateFirstInSequenceFor(sequence: List<Int>): Int =
         if (sequence.all { it == 0 }) 0
         else sequence.first() - calculateFirstInSequenceFor(sequence.zipWithNext { a, b -> b - a })
 
     private fun buildSequencesFrom(data: List<String>): List<List<Int>> =
-        data.map { it.split("\\s+".toRegex()).map { foo -> foo.toInt() } }
+        data.map {
+            it.split("\\s+".toRegex())
+                .map { foo -> foo.toInt() }
+        }
 }
