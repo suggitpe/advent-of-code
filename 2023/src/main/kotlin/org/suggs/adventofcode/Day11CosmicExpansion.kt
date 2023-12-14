df2131346815e12e172605476ee1a0ee521052bb
Peter Suggitt <me@suggs.org.uk>
2023-12-14 18:15:04 +0000

    refactored out the actual grid manipulation

--- 2023/src/main/kotlin/org/suggs/adventofcode/Day11CosmicExpansion.kt
+++ 2023/src/main/kotlin/org/suggs/adventofcode/Day11CosmicExpansion.kt
@@ -8,45 +8,43 @@ object Day11CosmicExpansion {
     private val log = LoggerFactory.getLogger(this::class.java)
 
     fun sumAllGalaxyDistancesIn(grid: Grid): Int =
-        findAllDistancesFromDiscreteCoordinatesIn(grid.expand()).sum()
+        findAllDistancesFromDiscreteCoordinatesIn(grid, grid.listRowsWithAllSameChar('.'), grid.listColumnsWithAllSameChar('.')).sum()
             .also { log.debug("Part 1: $it") }
 
-    private fun findAllDistancesFromDiscreteCoordinatesIn(grid: Grid): List<Int> =
-        createGalaxyRoutesFrom(grid.findAll('#'), listOf()).map { it.distance() }
+    private fun findAllDistancesFromDiscreteCoordinatesIn(grid: Grid, rowsWithAllSameChar: List<Int>, columnsWithAllSameChar: List<Int>): List<Int> =
+        createGalaxyRoutesFrom(grid.findAll('#'), listOf()).map { it.distance(rowsWithAllSameChar, columnsWithAllSameChar) }
 
     private fun createGalaxyRoutesFrom(coordinates: Set<Coordinate>, acc: List<GalaxyRoute>): List<GalaxyRoute> =
         if (coordinates.isEmpty()) acc
         else createGalaxyRoutesFrom(coordinates.drop(1).toSet(), acc + coordinates.drop(1).map { GalaxyRoute(coordinates.first(), it) }.toSet())
 
-    fun Grid.expand(): Grid = expandRows().expandColumns()
-
-    private fun Grid.expandRows(): Grid {
-        var newGridForRows = Grid.emptyGrid()
-        rows().forEach { row ->
-            newGridForRows = Grid(newGridForRows.appendRow(row))
-            if (row.all { it == '.' })
-                newGridForRows = Grid(newGridForRows.appendRow(CharArray(row.size) { '.' }))
+    private fun Grid.listRowsWithAllSameChar(char: Char): List<Int> {
+        val rows = mutableListOf<Int>()
+        rows().forEachIndexed { idx, row ->
+            if (row.all { it == char })
+                rows.add(idx)
         }
-        return newGridForRows
+        return rows
     }
 
-    private fun Grid.expandColumns(): Grid {
-        val expandedColumns = mutableListOf<CharArray>()
-        columns().forEach { column ->
-            expandedColumns.add(column)
-            if (column.all { it == '.' })
-                expandedColumns.add(CharArray(column.size) { '.' })
+    private fun Grid.listColumnsWithAllSameChar(char: Char): List<Int> {
+        val cols = mutableListOf<Int>()
+        columns().forEachIndexed { idx, col ->
+            if (col.all { it == '.' })
+                cols.add(idx)
         }
-        val foo = Grid(expandedColumns.toTypedArray())
-        val newGrid = mutableListOf<CharArray>()
-        expandedColumns[0].forEachIndexed { idx, _ ->
-            newGrid.add(foo.getColumnAt(idx))
-        }
-        return Grid(newGrid.toTypedArray())
+        return cols
     }
 
     data class GalaxyRoute(val from: Coordinate, val to: Coordinate) {
-        fun distance(): Int = abs(to.x - from.x) + abs(to.y - from.y)
+
+        fun distance(rowsWithAllSameChar: List<Int>, columnsWithAllSameChar: List<Int>): Int {
+            val xRange = if(from.x > to.x) to.x..from.x else from.x..to.x
+            val yRange = if(from.y > to.y) to.y..from.y else from.y..to.y
+            return (xRange.last - xRange.first) + xRange.filter { columnsWithAllSameChar.contains(it) }.size +
+                    (yRange.last - yRange.first) + yRange.filter { rowsWithAllSameChar.contains(it) }.size
+        }
+
     }
 
 }
