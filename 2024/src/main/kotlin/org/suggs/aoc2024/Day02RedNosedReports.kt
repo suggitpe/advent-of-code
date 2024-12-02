78a7355a0aba75bcb7c9f4e38f31acb7dccd643d
Peter Suggitt <me@suggs.org.uk>
2024-12-02 19:36:10 +0000

    updated a bit more ... tweak tweak

--- 2024/src/main/kotlin/org/suggs/aoc2024/Day02RedNosedReports.kt
+++ 2024/src/main/kotlin/org/suggs/aoc2024/Day02RedNosedReports.kt
@@ -7,20 +7,13 @@ object Day02RedNosedReports {
 
     private val log = LoggerFactory.getLogger(this::class.java)
 
-    fun countNumberOfSafeReportsFrom(data: List<String>): Int {
-        return data.count { isSafe(it.split(" ").map { it.toInt() }.zipWithNext().map { it.first - it.second }) }
-    }
+    fun countNumberOfSafeReportsFrom(data: List<String>) =
+        data.count { isSafe(it.split(" ").map { it.toInt() }.zipWithNext().map { it.first - it.second }) }
 
-    private fun isSafe(lineDeltas: List<Int>): Boolean {
-        return allInRange(lineDeltas) && sameDirection(lineDeltas)
-    }
+    private fun isSafe(lineDeltas: List<Int>) = allInRange(lineDeltas) && sameDirection(lineDeltas)
 
-    private fun allInRange(lineDeltas: List<Int>): Boolean {
-        return lineDeltas.all { abs(it) in 1..3 }
-    }
+    private fun allInRange(lineDeltas: List<Int>) = lineDeltas.all { abs(it) in 1..3 }
 
-    private fun sameDirection(lineDeltas: List<Int>): Boolean {
-        return lineDeltas.all { it > 0 } || lineDeltas.all { it < 0 }
-    }
+    private fun sameDirection(lineDeltas: List<Int>) = lineDeltas.all { it > 0 } || lineDeltas.all { it < 0 }
 
 }
