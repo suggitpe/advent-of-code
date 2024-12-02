f61e037348da8776f0096de7be6d411cf338597e
Peter Suggitt <me@suggs.org.uk>
2024-12-02 21:03:15 +0000

    set up for day 03

---
+++ 2024/src/test/kotlin/org/suggs/aoc2024/Day03Test.kt
@@ -0,0 +1,11 @@
+package org.suggs.aoc2024
+
+import org.junit.jupiter.api.DisplayName
+import org.suggs.adventofcode.Util.getFileLinesFrom
+
+@DisplayName("")
+class Day03Test {
+
+    private val largeData = getFileLinesFrom("day03-input.txt")
+    private val smallData = """""".split("\n")
+}
