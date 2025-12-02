ac81eaa6a26f7a5ecaa6530861c307e000c3dc09
Peter Suggitt <me@suggs.org.uk>
2025-12-02 21:04:15 +0000

    comleted day 2 part 1

--- 2025/src/main/kotlin/org/suggs/aoc2024/Day02GiftShop.kt
+++ 2025/src/main/kotlin/org/suggs/aoc2024/Day02GiftShop.kt
@@ -6,4 +6,43 @@ object Day02GiftShop {
 
     private val log = LoggerFactory.getLogger(this::class.java)
 
+    fun addAllInvalidIdsInRanges(smallData: List<String>, multiSplit: Boolean): Long =
+        smallData.sumOf { addAllInvalidIdsFrom(buildRangeFrom(it), multiSplit) }
+
+    fun addAllInvalidIdsFrom(range: LongRange, multiSplit: Boolean): Long {
+        return addAllInvalidIdsFrom(range.toList(), 0L, multiSplit)
+    }
+
+    private tailrec fun addAllInvalidIdsFrom(range: List<Long>, accu: Long, multiSplit: Boolean): Long {
+        return if (range.isEmpty()) {
+            accu
+        } else {
+            addAllInvalidIdsFrom(range.drop(1), accu + dodgyId(range.first(), multiSplit), multiSplit)
+        }
+    }
+
+    private fun dodgyId(number: Long, multiSplit: Boolean): Long {
+        return if(multiSplit) {
+            if (dodgyMultipleSplitId(number.toString())) number else 0
+        } else
+            if (dodgySingleSplitId(number.toString())) number else 0
+    }
+
+    private fun dodgyMultipleSplitId(num: String): Boolean {
+        return false
+    }
+
+    private fun dodgySingleSplitId(number: String): Boolean {
+        val mid = number.length / 2
+        return number.length >= 2 && number.length % 2 == 0 &&
+                number.take(mid) == number.substring(mid)
+    }
+
+    private fun buildRangeFrom(rangeData: String) =
+        rangeData
+            .split("-", limit = 2)
+            .takeIf { it.size == 2 }
+            .let { it!![0].toLong()..it[1].trim().toLong() }
+
+
 }
--- 2025/src/test/kotlin/org/suggs/aoc2024/Day02GiftShopTest.kt
+++ 2025/src/test/kotlin/org/suggs/aoc2024/Day02GiftShopTest.kt
@@ -1,10 +1,25 @@
 package org.suggs.aoc2024
 
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
 import org.suggs.adventofcode.Util.readFileAsString
+import org.suggs.aoc2024.Day02GiftShop.addAllInvalidIdsInRanges
 
 class Day02GiftShopTest {
 
+    @Test
+    fun `adds up all invalid ids from small sample data`(){
+        addAllInvalidIdsInRanges(smallData, false) shouldBe 1227775554L
+    }
+
+    @Test
+    @Disabled
+    fun `adds up all invalid ids from big sample data`(){
+        addAllInvalidIdsInRanges(largeData, false) shouldBe 1234L
+    }
+
     private val smallData =
-        "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
-    private val largeData = readFileAsString("day02-input.txt")
+        "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124".split(",")
+    private val largeData = readFileAsString("day02-input.txt").split(",")
 }
