7a72d6f04eaa212f9ceae80438751a11859a7ed6
Pete Suggitt <me@suggs.org.uk>
2025-12-03 11:51:41 +0000

    updated test names

--- 2025/src/test/kotlin/org/suggs/aoc2024/Day03LobbyTest.kt
+++ 2025/src/test/kotlin/org/suggs/aoc2024/Day03LobbyTest.kt
@@ -2,31 +2,33 @@ package org.suggs.aoc2024
 
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.suggs.adventofcode.Util.getFileLinesFrom
 import org.suggs.aoc2024.Day03Lobby.calculateTotalJoltageFrom
 
+@DisplayName("Day 03 - Lobby")
 class Day03LobbyTest {
 
     @Test
-    fun `calculates joltage from battery banks`() {
+    fun `calculates dual joltage banks from small number of battery banks`() {
         calculateTotalJoltageFrom(smallData, 2) shouldBe 357
     }
 
     @Test
     @Disabled
-    fun `calculates joltage from many battery banks`() {
+    fun `calculates dual joltage banks from large number of battery banks`() {
         calculateTotalJoltageFrom(largeData, 2) shouldBe 1234L
     }
 
     @Test
-    fun `calculates longer joltage from battery banks`() {
+    fun `calculates longer joltage banks from small number of battery banks`() {
         calculateTotalJoltageFrom(smallData, 12) shouldBe 3121910778619
     }
 
     @Test
     @Disabled
-    fun `calculates longer joltage from many battery banks`() {
+    fun `calculates longer joltage banks from large numbers of battery banks`() {
         calculateTotalJoltageFrom(largeData, 12) shouldBe 1234L
     }
 
