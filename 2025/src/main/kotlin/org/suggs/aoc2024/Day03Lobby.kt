b22f2dedb3f0e0e40950e0f2b49d0cd958e289ef
Peter Suggitt <me@suggs.org.uk>
2025-12-03 09:15:14 +0000

    tidy tidy

--- 2025/src/main/kotlin/org/suggs/aoc2024/Day03Lobby.kt
+++ 2025/src/main/kotlin/org/suggs/aoc2024/Day03Lobby.kt
@@ -1,23 +1,18 @@
 package org.suggs.aoc2024
 
-import org.slf4j.LoggerFactory
-
 object Day03Lobby {
-    private val log = LoggerFactory.getLogger(this::class.java)
 
     fun calculateTotalJoltageFrom(batteryBanks: List<String>, digitCount: Int): Long =
         batteryBanks.sumOf { findLargestBatteryBankFrom(it, digitCount, "") }
 
-    private tailrec fun findLargestBatteryBankFrom(batteries: String, digitCount: Int, numberString: String): Long {
-        return if(digitCount==0)
-            numberString.toLong()
-        else{
-            val maxValue = highesCharIn(batteries.dropLast(digitCount-1))
-            findLargestBatteryBankFrom(batteries.substringAfter(maxValue), digitCount-1, numberString+maxValue )
+    private tailrec fun findLargestBatteryBankFrom(batteries: String, digitCount: Int, numberString: String): Long =
+        if (digitCount == 0) numberString.toLong()
+        else {
+            val maxValue = highesCharIn(batteries.dropLast(digitCount - 1))
+            findLargestBatteryBankFrom(batteries.substringAfter(maxValue), digitCount - 1, numberString + maxValue)
         }
-    }
 
-    fun highesCharIn(bank: String): Char=
+    fun highesCharIn(bank: String): Char =
         bank.toList().sortedDescending().take(1).first()
 
 }
--- 2025/src/main/kotlin/org/suggs/aoc2024/Day04.kt
+++ 2025/src/main/kotlin/org/suggs/aoc2024/Day04.kt
@@ -1,4 +1,10 @@
 package org.suggs.aoc2024
 
+import org.slf4j.LoggerFactory
+
 object Day04 {
+
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+
 }
