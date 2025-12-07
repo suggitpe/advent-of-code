10128f2151f9470a514f48ce1e6ee1c65b0e9392
Peter Suggitt <me@suggs.org.uk>
2025-12-07 17:55:01 +0000

    updatedd

--- 2025/src/test/kotlin/org/suggs/aoc2024/Day07LaboratoriesTest.kt
+++ 2025/src/test/kotlin/org/suggs/aoc2024/Day07LaboratoriesTest.kt
@@ -3,12 +3,16 @@ package org.suggs.aoc2024
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import org.suggs.adventofcode.Coordinate
 import org.suggs.adventofcode.Grid
 import org.suggs.adventofcode.Util.createGridFromFileContent
 import org.suggs.aoc2024.Day07Laboratories.countSplitterEncountersFrom
 import org.suggs.aoc2024.Day07Laboratories.findUniqueRoutesFor
 
 class Day07LaboratoriesTest {
+
+    private val log = LoggerFactory.getLogger(this::class.java)
 
     @Test
     fun `counts the splitter encounters - very small`() {
@@ -40,6 +44,18 @@ class Day07LaboratoriesTest {
     @Disabled
     fun `counts the unique routes - large`() {
         findUniqueRoutesFor(largeGrid) shouldBe 1234L
+    }
+
+    @Test
+    @Disabled
+    fun `let tests`(){
+        val coord = Coordinate(1,1)
+        val hash = mutableMapOf<Coordinate, Long>()
+        hash[coord]?.let{log.debug("first $it ... should not run")}
+        if(hash.containsKey(coord)){ log.debug("this should also not work")}
+        hash[coord] = 4L
+        hash[coord]?.let{log.debug("second $it should run")}
+        if(hash.containsKey(coord)){ log.debug("this should work")}
     }
 
     private val largeGrid = createGridFromFileContent("day07-input.txt")
